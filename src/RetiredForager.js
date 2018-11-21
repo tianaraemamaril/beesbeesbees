5c4ada73701d121dab1b5846c078546cffb69b85
Tiana Rae Mamaril <tianaraetran@gmail.com>
2018-11-21 12:21:07 -0800

    Refactored out unnecessary code.

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,28 @@
 var RetiredForagerBee = function() {
+
+	ForagerBee.call(this);
+
+	this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+ 
 };
+
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+  this.forage = 'I am too old, let me play cards instead';
+  return this.forage;
+}
+RetiredForagerBee.prototype.gamble = function(wonTreasures) {
+  this.treasureChest.push(wonTreasures);
+}
+
+
+
+
+
+
 
